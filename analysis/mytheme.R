62dd84642cc3c49167fc1200915c71e7e6739b60
Jiao <JiaoWu2020@gmail.com>
2024-07-01 07:40:01 +0200

    reconstructed the figures

--- analysis/mytheme.R
+++ analysis/mytheme.R
@@ -3,6 +3,7 @@ colorSet3 <- scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))
 colorSet4 <- scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9", "#1a9641")) 
 colorSet <- scale_color_manual(values=c("#d7191c", "#fdae61", "#a6d96a", "#1a9641")) 
 colorSet5 <- scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9", "#1a9641", "#d7191c")) 
+fillSet3 <- scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"))
 
 #customize theme
 theme_new <- theme_bw() + 
